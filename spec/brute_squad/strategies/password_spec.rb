85a19d866391ffe01930e4df9a110791fba2d6e8
Matt Powell <fauxparse@gmail.com>
2010-04-13 09:12:30 +1200

    test for invalid login

--- spec/brute_squad/strategies/password_spec.rb
+++ spec/brute_squad/strategies/password_spec.rb
@@ -20,53 +20,70 @@ describe BruteSquad::Strategies::Password do
   end
   
   describe "logging in from the app" do
-    before :each do
-      @env = env_with_params
-      @rack = setup_rack do |env|
-        env["brute_squad"].users.attempt_login :email => "test@example.com", :password => "password"
-        success_app.call env
-      end
-    end
-    
-    it "should allow logins" do
-      @rack.call @env
-      @env["brute_squad"].users.current.should == @user
-    end
-    
-    it "should set a valid cookie" do
-      s, h, r = @rack.call @env
-      cookies = parse_cookies(h)
-      cookies["brute_squad.users.session"].should be_present
-      
-      env = env_with_params "/", {}, { 'HTTP_COOKIE' => "brute_squad.users.session=#{cookies["brute_squad.users.session"]};" }
-      setup_rack(success_app).call(env)
-      env["brute_squad"].users.current.should == @user
-    end
-    
-    describe "and logging out again" do
+    describe "with a valid username and password" do
       before :each do
-        s, h, r = @rack.call @env
-        cookies = parse_cookies(h)
-        @env = env_with_params "/", {}, { 'HTTP_COOKIE' => "brute_squad.users.session=#{cookies["brute_squad.users.session"]};" }
+        @env = env_with_params
         @rack = setup_rack do |env|
-          env["brute_squad"].users.log_out_and_redirect! "/login"
+          env["brute_squad"].users.attempt_login :email => "test@example.com", :password => "password"
           success_app.call env
         end
+      end
+    
+      it "should allow logins" do
+        @rack.call @env
+        @env["brute_squad"].users.current.should == @user
+      end
+    
+      it "should set a valid cookie" do
+        s, h, r = @rack.call @env
+        cookies = parse_cookies(h)
+        cookies["brute_squad.users.session"].should be_present
+      
+        env = env_with_params "/", {}, { 'HTTP_COOKIE' => "brute_squad.users.session=#{cookies["brute_squad.users.session"]};" }
+        setup_rack(success_app).call(env)
+        env["brute_squad"].users.current.should == @user
+      end
+    
+      describe "and logging out again" do
+        before :each do
+          s, h, r = @rack.call @env
+          cookies = parse_cookies(h)
+          @env = env_with_params "/", {}, { 'HTTP_COOKIE' => "brute_squad.users.session=#{cookies["brute_squad.users.session"]};" }
+          @rack = setup_rack do |env|
+            env["brute_squad"].users.log_out_and_redirect! "/login"
+            success_app.call env
+          end
         
-        @status, @headers, @response = @rack.call(@env)
+          @status, @headers, @response = @rack.call(@env)
+        end
+      
+        it "should delete the cookie" do
+          cookies = parse_cookies(@headers)
+        end
+      
+        it "should redirect to another URL" do
+          @status.to_i.should == 302
+          @headers["Location"].should == "/login"
+        end
+      
+        it "should send a proper message" do
+          @response.first.should == "You have been logged out."
+        end
+      end
+    end
+    
+    describe "with a bad password" do
+      before :each do
+        @env = env_with_params
+        @rack = setup_rack do |env|
+          env["brute_squad"].users.attempt_login :email => "test@example.com", :password => "bad password"
+          success_app.call env
+        end
       end
       
-      it "should delete the cookie" do
-        cookies = parse_cookies(@headers)
-      end
-      
-      it "should redirect to another URL" do
-        @status.to_i.should == 302
-        @headers["Location"].should == "/login"
-      end
-      
-      it "should send a proper message" do
-        @response.first.should == "You have been logged out."
+      it "should not allow logins" do
+        @rack.call @env
+        @env["brute_squad"].users.should_not be_logged_in
       end
     end
   end
